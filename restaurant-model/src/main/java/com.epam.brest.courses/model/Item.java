89df62893930a297e6d02005fbb099b704dcaa4b
MrKlimuk <mr.d.klimuk@gmail.com>
2020-03-24 15:59:17 +0300

    add Item.java

---
+++ restaurant-model/src/main/java/com.epam.brest.courses/model/Item.java
@@ -0,0 +1,45 @@
+package com.epam.brest.courses.model;
+
+import java.math.BigDecimal;
+
+public class Item {
+
+    private Integer itemId;
+    private String itemName;
+    private BigDecimal itemPrice;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "itemId=" + itemId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                '}';
+    }
+}
+
+
