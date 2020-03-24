7129479d7e151bd1b7796db3af98c794dcc19052
MrKlimuk <mr.d.klimuk@gmail.com>
2020-03-24 16:27:45 +0300

    add conf file: create-db.sql + test-db.xml, set maven

---
+++ restaurant-test-db/src/main/resources/create-db.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS ordertable;
+CREATE TABLE ordertable (
+  order_id INT NOT NULL AUTO_INCREMENT,
+  order_name VARCHAR(20) NOT NULL UNIQUE,
+  order_price INT,
+  PRIMARY KEY (order_id)
+);
+
+DROP TABLE IF EXISTS item;
+CREATE TABLE item(
+    item_id INT NOT NULL AUTO_INCREMENT,
+    item_name VARCHAR(80) NOT NULL UNIQUE,
+    item_price INT NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+
