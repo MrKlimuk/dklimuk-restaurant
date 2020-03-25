cd9af67e37e2f45f6f7eddaf28ee83c68d06efd6
MrKlimuk <mr.d.klimuk@gmail.com>
2020-03-25 21:32:02 +0300

    add items.html with finAll items

--- restaurant-web-app/src/main/java/com/epam/brest/courses/web_app/ItemController.java
+++ restaurant-web-app/src/main/java/com/epam/brest/courses/web_app/ItemController.java
@@ -1,15 +1,35 @@
 package com.epam.brest.courses.web_app;
 
+import com.epam.brest.courses.model.Item;
+import com.epam.brest.courses.service.ItemService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
 
     @GetMapping(value = "/")
     public String goToHelloPage(){
         return "hello";
     }
+
+    @GetMapping(value = "/items")
+    public String goToItemsPage(Model model){
+        List<Item> items = itemService.findAllItem();
+        model.addAttribute("items", items);
+        return "items";
+    }
+
+
 
 
 }
