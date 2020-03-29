b5abcecd704b224d7c5f8a56f03f287060194f8c
MrKlimuk <mr.d.klimuk@gmail.com>
2020-03-29 23:37:54 +0300

    OrderController: add method addOrder

--- restaurant-web-app/src/main/java/com/epam/brest/courses/web_app/OrderController.java
+++ restaurant-web-app/src/main/java/com/epam/brest/courses/web_app/OrderController.java
@@ -3,12 +3,13 @@ package com.epam.brest.courses.web_app;
 
 import com.epam.brest.courses.model.Order;
 import com.epam.brest.courses.service.OrderService;
-import jdk.vm.ci.meta.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -32,4 +33,17 @@ public class OrderController {
         orderService.delete(orderId);
         return "redirect:/orders";
     }
+
+    @GetMapping(value = "/order/add")
+    public String goToAddOrder(){
+
+    return "orderAdd";
+    }
+
+    @PostMapping(value = "/orderAdd")
+    public String addOrder(@Valid Order order){
+        orderService.createOrder(order);
+        return "redirect:/orders";
+    }
+
 }
