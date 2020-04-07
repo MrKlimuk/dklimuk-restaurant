abb26a1d95ea72255d4869c8248b0410d294f4fd
MrKlimuk <mr.d.klimuk@gmail.com>
2020-04-07 22:36:54 +0300

    add VersionController + test

---
+++ restaurant-rest-app/src/main/java/com/epam/brest/courses/VersionController.java
@@ -0,0 +1,16 @@
+package com.epam.brest.courses;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+
+    private final static String VERSION = "0.0.1";
+
+    @GetMapping(value = "/version")
+    public String version(){
+        return VERSION;
+    }
+}
