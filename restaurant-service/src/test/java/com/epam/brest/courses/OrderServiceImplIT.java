0cb158801481446b19b665f086f346f1af2ff966
MrKlimuk <mr.d.klimuk@gmail.com>
2020-04-15 21:19:01 +0300

    complete OrderServiceImplIT

--- restaurant-service/src/test/java/com/epam/brest/courses/OrderServiceImplIT.java
+++ restaurant-service/src/test/java/com/epam/brest/courses/OrderServiceImplIT.java
@@ -3,22 +3,30 @@ package com.epam.brest.courses;
 
 import com.epam.brest.courses.model.Order;
 import com.epam.brest.courses.service.OrderService;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.epam.brest.courses.constants.OrderConstants.ORDER_NAME_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith({SpringExtension.class})
 @ContextConfiguration({"classpath*:test-db.xml", "classpath*:test-service.xml", "classpath:dao.xml"})
 public class OrderServiceImplIT {
 
     private final OrderService orderService;
+    private static BigDecimal PRICE = new BigDecimal(0);
+    private static BigDecimal PRICE_FOR_UPDATE = new BigDecimal(50);
+
 
     @Autowired
     public OrderServiceImplIT(OrderService orderService) {
@@ -30,5 +38,71 @@ public class OrderServiceImplIT {
         List<Order> orders = orderService.findAllOrders();
         assertNotNull(orders);
         assertTrue(orders.size() > 0);
+    }
+
+    @Test
+    public void shouldFindOrderById(){
+        Order order = new Order()
+                .setOrderName(RandomStringUtils.randomAlphabetic(ORDER_NAME_SIZE))
+                .setOrderPrice(PRICE);
+
+        Integer id = orderService.createOrder(order);
+        Optional<Order> orderOptional = orderService.findOrderById(id);
+
+        Assertions.assertTrue(orderOptional.isPresent());
+        assertEquals(orderOptional.get().getOrderId(), id);
+        assertEquals(orderOptional.get().getOrderName(), order.getOrderName());
+        assertEquals(orderOptional.get().getOrderPrice(), order.getOrderPrice());
+    }
+
+    @Test
+    public void shouldCreateOrder(){
+        Order order = new Order()
+                .setOrderName(RandomStringUtils.randomAlphabetic(ORDER_NAME_SIZE));
+        Integer id = orderService.createOrder(order);
+        assertNotNull(id);
+    }
+
+    @Test
+    public void shouldUpdateOrder(){
+        Order order = new Order()
+                .setOrderName(RandomStringUtils.randomAlphabetic(ORDER_NAME_SIZE))
+                .setOrderPrice(PRICE);
+        Integer id = orderService.createOrder(order);
+        assertNotNull(id);
+
+        Optional<Order> orderOptional = orderService.findOrderById(1);
+        Assertions.assertTrue(orderOptional.isPresent());
+
+        orderOptional.get().setOrderName(RandomStringUtils.randomAlphabetic(ORDER_NAME_SIZE));
+
+        int result = orderService.update(orderOptional.get());
+
+        assertTrue(1 == result);
+
+        Optional<Order> updateOrderOptional  =orderService.findOrderById(1);
+        Assertions.assertTrue(updateOrderOptional.isPresent());
+        assertEquals(updateOrderOptional.get().getOrderId(), (Integer) 1);
+        assertEquals(updateOrderOptional.get().getOrderName(), orderOptional.get().getOrderName());
+        assertEquals(updateOrderOptional.get().getOrderPrice(), orderOptional.get().getOrderPrice());
+
+    }
+
+    @Test
+    public void shouldDeleteOrder(){
+        Order order = new Order().
+                setOrderName(RandomStringUtils.randomAlphabetic(ORDER_NAME_SIZE));
+        Integer id = orderService.createOrder(order);
+
+        List<Order> orders = orderService.findAllOrders();
+        assertNotNull(orders);
+
+        int result = orderService.delete(id);
+        assertTrue(1 == result);
+
+        List<Order> currentOrder = orderService.findAllOrders();
+        assertNotNull(currentOrder);
+
+        assertTrue(orders.size() - 1 == currentOrder.size());
     }
 }
