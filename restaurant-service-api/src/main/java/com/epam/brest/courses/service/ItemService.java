060ca8fbe85d623821d32ec735da27dcf58d69a4
MrKlimuk <mr.d.klimuk@gmail.com>
2020-04-30 12:12:30 +0300

    Add Service-api descriptions.

--- restaurant-service-api/src/main/java/com/epam/brest/courses/service/ItemService.java
+++ restaurant-service-api/src/main/java/com/epam/brest/courses/service/ItemService.java
@@ -5,10 +5,47 @@ import com.epam.brest.courses.model.Item;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The interface provides access to service methods.
+ */
 public interface ItemService {
+
+    /**
+     * Find all items.
+     *
+     * @return item list.
+     */
     List<Item> findAllItem();
+
+    /**
+     * Find item by Id.
+     *
+     * @param itemId item Id.
+     * @return item.
+     */
     Optional<Item> findItemById(Integer itemId);
+
+    /**
+     * Creates new item.
+     *
+     * @param item item.
+     * @return created item id.
+     */
     Integer createItem(Item item);
+
+    /**
+     * Update item.
+     *
+     * @param item item.
+     * @return number of updated records in the database.
+     */
     int updateItem(Item item);
+
+    /**
+     * Delete item.
+     *
+     * @param itemId item Id.
+     * @return the number of rows affected.
+     */
     int deleteItem(Integer itemId);
 }
--- restaurant-service-api/src/main/java/com/epam/brest/courses/service/PositionService.java
+++ restaurant-service-api/src/main/java/com/epam/brest/courses/service/PositionService.java
@@ -5,12 +5,55 @@ import com.epam.brest.courses.model.Position;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The interface provides access to service methods.
+ */
 public interface PositionService {
 
+    /**
+     * Find all positions.
+     *
+     * @return positions list.
+     */
     List<Position> findAllPosition();
+
+    /**
+     * Find position by id.
+     *
+     * @param positionId position id.
+     * @return position.
+     */
     Optional<Position> findPositionById(Integer positionId);
+
+    /**
+     *  Find positions by order id.
+     *
+     * @param positionOrderId position order id.
+     * @return positions list.
+     */
     List<Position> findPositionByOrderId(Integer positionOrderId);
+
+    /**
+     * Create position.
+     *
+     * @param position position.
+     * @return created position id.
+     */
     Integer create(Position position);
+
+    /**
+     * Update position.
+     *
+     * @param position position.
+     * @return number of updated records in the database.
+     */
     int update(Position position);
+
+    /**
+     * Delete position by Id.
+     *
+     * @param positionId position Id.
+     * @return the number of rows affected.
+     */
     int delete(Integer positionId);
 }
