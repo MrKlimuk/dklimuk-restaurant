64efb2b1149773fcb7f10cbe244af8442c465665
MrKlimuk <mr.d.klimuk@gmail.com>
2020-06-07 19:20:37 +0300

    dao with Spring Boot

---
+++ restaurant-rest-app/src/main/java/com/epam/brest/courses/rest/App.java
@@ -0,0 +1,21 @@
+package com.epam.brest.courses.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(scanBasePackages = {"com.epam.brest.courses*"})
+@ImportResource(locations = "classpath*:app-context.xml")
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
