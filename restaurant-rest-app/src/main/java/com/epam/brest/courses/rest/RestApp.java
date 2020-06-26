306e099a2e8207ed6633c6efad9c827b1611c0be
MrKlimuk <mr.d.klimuk@gmail.com>
2020-06-26 22:35:41 +0300

    add prod profile with mysql db

--- restaurant-rest-app/src/main/java/com/epam/brest/courses/rest/RestApp.java
+++ restaurant-rest-app/src/main/java/com/epam/brest/courses/rest/RestApp.java
@@ -1,14 +1,15 @@
 package com.epam.brest.courses.rest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@RestController
+@SpringBootConfiguration
 @SpringBootApplication(scanBasePackages = {"com.epam.brest.courses*"})
-@ImportResource(locations = {"classpath*:test-db.xml", "classpath*:dao.xml"})
+@ImportResource(locations = {"classpath*:prod-db.xml", "classpath*:test-db.xml", "classpath*:dao.xml"})
 public class RestApp {
     public static void main(String[] args) {
 
